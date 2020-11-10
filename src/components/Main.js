b8241c698f95563334a0920b84715b20b9338521
unknown <abdelhakim@univ-tiaret.dz>
2020-11-10 20:13:15 +0100

    main.js file

---
+++ src/components/Main.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Nav = () => {
+    return(
+        <div className="navBar">
+          
+        </div>
+
+    )
+
+}
+export default Nav
+//i'm tired i can't learn or coding 
