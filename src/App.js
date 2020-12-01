d097633e3e5410702a0f87ee597151c60d0daa55
unknown <abdelhakim@univ-tiaret.dz>
2020-12-01 21:03:42 +0100

    d

--- src/App.js
+++ src/App.js
@@ -73,6 +73,7 @@ function App() {
                     
                     <div className="cardContent">
                       <h3>Music for concentration</h3>
+                      <p className="subTextsmall">Music to help you concentrate</p>
                      
 
 
